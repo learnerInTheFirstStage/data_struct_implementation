01eeff8faf2d3142017091d02953df89d4af7a6a
Your Name <you@example.com>
2024-04-03 07:57:43 -0700

    update Test.java

--- LRU_Algorithm/src/Test.java
+++ LRU_Algorithm/src/Test.java
@@ -1,14 +1,25 @@
 public class Test {
     public static void main(String[] args) {
-        LRUCache lruCache = new LRUCache(2);
-        lruCache.put(1, 1);
-        lruCache.put(2, 2);
-        System.out.println(lruCache.get(1)); // return 1
-        lruCache.put(3, 3);
-        System.out.println(lruCache.get(2)); // return -1
-        lruCache.put(4, 4);
-        System.out.println(lruCache.get(1)); // return -1
-        System.out.println(lruCache.get(3)); // return 3
-        System.out.println(lruCache.get(4)); // return 4
+//        LRUCache lruCache = new LRUCache(2);
+//        lruCache.put(1, 1);
+//        lruCache.put(2, 2);
+//        System.out.println(lruCache.get(1)); // return 1
+//        lruCache.put(3, 3);
+//        System.out.println(lruCache.get(2)); // return -1
+//        lruCache.put(4, 4);
+//        System.out.println(lruCache.get(1)); // return -1
+//        System.out.println(lruCache.get(3)); // return 3
+//        System.out.println(lruCache.get(4)); // return 4
+
+        LRUCacheII lruCacheII = new LRUCacheII(2);
+        lruCacheII.put(1, 1);
+        lruCacheII.put(2, 2);
+        System.out.println(lruCacheII.get(1)); // 1
+        lruCacheII.put(3, 3);
+        System.out.println(lruCacheII.get(2)); // -1
+        lruCacheII.put(4, 4);
+        System.out.println(lruCacheII.get(1)); // -1
+        System.out.println(lruCacheII.get(3)); // 3
+        System.out.println(lruCacheII.get(4)); // 4
     }
 }
